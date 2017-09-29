5a4f9963d6dc7ff492a3a54cd428d31728a1e779
Mike Wilcox <anm9tr@gmail.com>
2017-09-29 08:11:37 -0500

    Add dom to dist

--- scripts/compile.js
+++ scripts/compile.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const prefix = `(function () {
 `;
 const suffix = `
+	window.dom = dom;
 	window.on = on;
 	window.mouse = mouse;
 }());`;
 
 function compile () {
 	const onFile = fs.readFileSync('./node_modules/@clubajax/on/src/on.js').toString().split('return on;')[0].split('\'use strict\';')[1];
+	const domFile = fs.readFileSync('./node_modules/@clubajax/dom/src/dom.js').toString().split('return dom;')[0].split('\'use strict\';')[1];
 	const mouseFile = fs.readFileSync('./src/mouse.js').toString().split('return mouse;')[0].split('\'use strict\';')[1];
-	const file = `${prefix}${onFile}${mouseFile}${suffix}`;
+	const file = `${prefix}${onFile}${domFile}${mouseFile}${suffix}`;
 	fs.writeFileSync('./dist/mouse.js', file);
 }
 
